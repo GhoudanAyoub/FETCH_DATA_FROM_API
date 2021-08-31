9d2d25a0277faffc828f1d27b834d53b920b9379
ayoub <ayoubghoudanos@gmail.com>
2021-08-31 11:43:38 +0100

    payment card method

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:json_api/API/second.dart';
+import 'package:flutter_rave/flutter_rave.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,7 +11,88 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: UserList(),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Builder(
+            builder: (context) => SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10.0, right: 10),
+                child: InkWell(
+                  onTap: () => pay(context),
+                  child: Card(
+                    color: Colors.orangeAccent,
+                    elevation: 15,
+                    child: Container(
+                      height: 250,
+                      width: MediaQuery.of(context).size.width,
+                      child: Center(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              "Card Payment",
+                              style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              width: 10,
+                            ),
+                            Icon(
+                              Icons.payment,
+                              color: Colors.black,
+                              size: 30,
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
+  }
+
+  pay(BuildContext context, {bool mounted = false}) {
+    RaveCardPayment(
+      isDemo: true,
+      encKey: "c53e399709de57d42e2e36ca",
+      publicKey: "FLWPUBK-d97d92534644f21f8c50802f0ff44e02-X",
+      transactionRef: "hvHPvKYaRuJLlJWSPWGGKUyaAfWeZKnm",
+      amount: 100,
+      email: "demo1@example.com",
+      onSuccess: (response) {
+        print("$response");
+        print("Transaction Successful");
+
+        if (mounted) {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text("Transaction Sucessful!"),
+              backgroundColor: Colors.green,
+              duration: Duration(
+                seconds: 5,
+              ),
+            ),
+          );
+        }
+      },
+      onFailure: (err) {
+        print("$err");
+        print("Transaction failed");
+      },
+      onClosed: () {
+        print("Transaction closed");
+      },
+      context: context,
+    ).process();
   }
 }
